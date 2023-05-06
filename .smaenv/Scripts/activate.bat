84715054329c7de4fcae62617860be3641047b00
andreishr <andreighiteanu1@gmail.com>
2023-05-07 00:04:53 +0300

    Incomplete PathFind

--- .smaenv/Scripts/activate.bat
+++ .smaenv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\andre\OneDrive\Desktop\projects\sma\.smaenv
+set VIRTUAL_ENV=C:\Users\andre\OneDrive\Desktop\projects\SMA_Proj\.smaenv
 
 if not defined PROMPT set PROMPT=$P$G
 
